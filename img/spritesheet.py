8e9d681f60e48136ca1396adae0fcc8ab002aa6c
Amy <amyemartyn@gmail.com>
2017-10-30 23:13:40 -0700

    adding spritesheet

---
+++ img/spritesheet.py
@@ -0,0 +1,20 @@
+import glob
+
+from PIL import Image
+
+images = []
+for file in glob.glob('*.png'):
+	if file.startswith('Be'):
+		img = Image.open(file)
+		images.append(img)
+
+width = max(x.width for x in images)
+height = max(x.height for x in images)
+sheet = Image.new('RGBA', (width * len(images), height))
+print(width, height)
+x = 0
+for image in images:
+	sheet.paste(image, (x, 0, x + image.width, image.height))
+	x += width
+sheet = sheet.resize((sheet.width // 10, sheet.height // 10), Image.BICUBIC)
+sheet.save('bennett.png')
